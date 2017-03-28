19721cf32eb9ac35062a60bb4279e9d64c6ba66d
Denys Bykanov <denys.bykanov@globallogic.com>
2017-03-28 12:57:37 +0300

    02_BarrelFiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { List1Component } from  './list1/list1.component';
-import { List2Component } from  './list2/list2.component';
+import { ListsModule, routs } from './lists/index';
 
 @NgModule({
-  imports:      [
-      BrowserModule,
-      RouterModule.forRoot([ // настройка маршрутов
-          { path: 'list1', component: List1Component },
-          { path: 'list2', component: List2Component },
-          { path: '', redirectTo: 'list1', pathMatch: 'full' }
-      ])
-  ],
-  declarations: [ AppComponent, List1Component, List2Component ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        ListsModule,
+        RouterModule.forRoot(routs)
+    ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
